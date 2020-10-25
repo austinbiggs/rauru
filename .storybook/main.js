d5dfbc066422ecfdcd9694f481f464d90ecdc129
Austin Biggs <austin@codesage.io>
2020-10-25 10:46:21 -0600

    Setup SCSS and Typesscript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,6 +8,28 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    {
+      name: "@storybook/preset-scss",
+      options: {
+        cssLoaderOptions: {
+          modules: true,
+          localIdentName: "[name]__[local]--[hash:base64:5]",
+        }
+      }
+    },
+    {
+      name: "@storybook/preset-typescript",
+      options: {
+        tsLoaderOptions: {
+          configFile: path.resolve(__dirname, "./tsconfig.json"),
+        },
+        forkTsCheckerWebpackPluginOptions: {
+          colors: false, // disables built-in colors in logger messages
+        },
+        include: [path.resolve(__dirname, "../src")],
+        transpileManager: true,
+      },
+    },
   ]
 }
